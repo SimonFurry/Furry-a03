b481806792b0935ab28e8c2709403555de0f3cf9
Simon <simonfurry@gmail,.com>
2021-09-23 18:37:28 -0400

    first commit

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,36 @@
+package baseline;/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+
+import java.util.Scanner;
+
+public class Solution24 {
+    /*
+    Create an anagram checker.
+    Take 2 string inputs
+    determine if 2 strings are anagrams
+
+    implement the program using a function called 'isAnagram'
+    isAnagram will take two words as its arguments and
+    return true or false
+     */
+    private static final Scanner in = new Scanner(System.in);
+
+    static void isAnagram(){
+    System.out.print("");
+    System.out.print("");
+    String firstString = in.next();
+    System.out.print("");
+    String secondString = in.next();
+
+    if(){
+        System.out.print();
+    }else(){
+        System.out.print();
+    }
+    }
+    public static void main(String[] args){
+       isAnagram();
+    }
+}
