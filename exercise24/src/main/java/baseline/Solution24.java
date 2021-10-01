93a1661c4a473ec4c411fe1deb03e8f4f1400ad8
Simon <simonfurry@gmail,.com>
2021-10-01 11:06:21 -0400

    second commit

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -1,9 +1,11 @@
-package baseline;/*
+package baseline;
+/*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Simon Furry
  */
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Solution24 {
     /*
@@ -17,20 +19,45 @@ public class Solution24 {
      */
     private static final Scanner in = new Scanner(System.in);
 
-    static void isAnagram(){
-    System.out.print("");
-    System.out.print("");
-    String firstString = in.next();
-    System.out.print("");
-    String secondString = in.next();
-
-    if(){
-        System.out.print();
-    }else(){
-        System.out.print();
-    }
-    }
     public static void main(String[] args){
-       isAnagram();
+        isAnagram();
     }
-}
+//* Create a function that...
+    //* Reads the user input and decides if it is or isn't an anagram.
+    //* Mock them relentlessly in output about their failure, I have no soul.
+    static void isAnagram(){
+        //Inputs from user.
+        System.out.print("This program will tell you if two words are Anagrams:\n");
+        System.out.print("Please enter your first word: ");
+        String userStr1 = in.next();
+        System.out.print("Please enter your second word: ");
+        String userStr2 = in.next();
+
+        //Convert all characters to lowercase (so R = r not R != r).
+            userStr1 = userStr1.toLowerCase();
+            userStr2 = userStr2.toLowerCase();
+
+            /*
+            Check if entered string has = length
+            if not = length, then false.
+            if = length, then true.
+            */
+                if(userStr1.length() == userStr2.length()) {
+                    //convert strings to char array
+                    char[] charArray1 = userStr1.toCharArray();
+                    char[] charArray2 = userStr2.toCharArray();
+                    //sort the array
+                    Arrays.sort(charArray1);
+                    Arrays.sort(charArray2);
+
+                    boolean trueResult = Arrays.equals(charArray1, charArray2);
+
+                    if(trueResult) {
+                        System.out.println(userStr1 + " and " + userStr2 + " are an Anagram.");
+                    }
+                    else {
+                        System.out.println(userStr1 + " and " + userStr2 + " are not an Anagram.");
+                    }
+                }
+            }
+        }
