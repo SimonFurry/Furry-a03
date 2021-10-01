38eade49eca090d30e178c62ef4ed7c172236af1
Simon <simonfurry@gmail,.com>
2021-10-01 18:48:18 -0400

    first commit of exercise27

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,18 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+import java.util.Scanner;
+
+public class Solution27 {
+    /*
+    Write a program that:
+    prompts for first name, last name, employee ID, and ZIP code
+    The first name must be filled in.
+    The last name must be filled in.
+    The first and last names must be at least two characters long.
+    An employee ID is in the format AA-1234. So, two letters, a hyphen, and four numbers.
+    The ZIP code must be a number.
+     */
+}
