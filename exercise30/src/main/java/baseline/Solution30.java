7c50b13c4d9f39df13be63313c848ca26530d20e
Simon <simonfurry@gmail,.com>
2021-10-02 12:33:23 -0400

    initial commit of exercise30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,20 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+import java.util.Scanner;
+
+public class Solution30 {
+    public static void main(String[] args){
+        generateTimesTable();
+    }
+    static void generateTimesTable(){
+        /*
+        generate code that will:
+        create rows and collums based on 2 integers.
+        (12*12)
+        Wow that pseudocode was kinda simple, what's the worst that could happen? :)
+         */
+    }
+}
