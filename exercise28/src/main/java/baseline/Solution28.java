4cd4e749be55d7a490d2a9cc6e9dffdedf9370d9
Simon <simonfurry@gmail,.com>
2021-10-01 21:42:18 -0400

    Second commit of exercise28

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -4,15 +4,27 @@ package baseline;
  *  Copyright 2021 Simon Furry
  */
 import java.util.Scanner;
+
 public class Solution28 {
     /*
     Prompt user for five numbers
     computes total of the numbers
      */
-    public static void main(String[] args){
+    private static final Scanner in = new Scanner(System.in);
 
+    public static void main(String[] args){
+        counter();
     }
     static void counter(){
+        int num;
+        double sum = 0;
 
+        for(int amount = 5; amount > 0; amount--){
+            System.out.print("Enter a number: ");
+            num = in.nextInt();
+
+            sum = sum + num;
+        }
+        System.out.print("The total is " + sum + ".");
     }
 }
