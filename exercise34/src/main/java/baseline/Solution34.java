e264a8acfb806075072652b52c5f5b8e17af92bb
Simon <simonfurry@gmail,.com>
2021-10-02 19:01:07 -0400

    second commit of exercise34

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -3,6 +3,7 @@ package baseline;
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Simon Furry
  */
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution34 {
@@ -13,10 +14,34 @@ public class Solution34 {
     Prompt for an employee name and remove that name from the list.
     display remaining employees.
      */
-    public static void main(String[] args){
+    private static final Scanner in = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        employeeListRemoval();
     }
-    static void employeeListRemoval(){
 
+    static void employeeListRemoval() {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("John Smith"); //List of names
+        names.add("Jackie Jackson");
+        names.add("Chris Jones");
+        names.add("Amanda Cullen");
+        names.add("Jeremy Goodwin");
+
+        System.out.print("There are " + names.size() + " employees.\n");
+        for (String name : names) {//Loop to print out each individual name
+            System.out.println(name);
+        }
+
+        System.out.print("\nEnter an employee name to remove: ");
+        String userInput = in.nextLine();//take user input for which name to remove.
+
+
+        names.remove(userInput);//get that punk ass name on outta here.
+
+        System.out.print("\nThere are " + names.size() + " employees.\n");
+        for (String name : names) {//print out new list of names without that losers name on it.
+            System.out.println(name);
+        }
     }
 }
