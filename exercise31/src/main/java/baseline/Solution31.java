53eb50f552a8fb8790c977cf2e73068d45114293
Simon <simonfurry@gmail,.com>
2021-10-02 14:59:59 -0400

    second commit of excercise31

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Solution31 {
     private static final Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         /*
         Take user input of:
         age = in.newInt();
@@ -17,11 +17,24 @@ public class Solution31 {
         intensity = 55 (loop) (55 + 5) until intensity = 95
         obtain targetHeartRate:
         targetHeartRate = (((220 - age) - restingHeartRate) * intensity) + restingHeartRate;
-
          */
+        generateKarvonenHeartRate();
     }
-    public static int[][] generateKarvonenHeartRate(){
 
-        return yes;
+    public static void generateKarvonenHeartRate() {
+        System.out.print("Resting Pulse: ");
+        int rp = in.nextInt();
+        System.out.print("Age: ");
+        int age = in.nextInt();
+        System.out.print("--------------------\n");
+        int intensity = 55;
+        while (intensity < 100) {
+            double intensityPercentage = intensity * .01;
+            double targetHeartRate = (((220 - age) - rp) * intensityPercentage) + rp;
+            double targetHeartRateRounded = targetHeartRate * 100/100;
+            System.out.print(intensity + "%\t\t|\t" + Math.round(targetHeartRateRounded) + " bpm\n");
+            intensity = intensity + 5;
+        }
+
     }
 }
