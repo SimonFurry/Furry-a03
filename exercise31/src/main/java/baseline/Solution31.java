10d08d1d610d901ad4952fd87221cc36ecf2f70b
Simon <simonfurry@gmail,.com>
2021-10-02 13:24:09 -0400

    initial commit for exercise31

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,27 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+import java.util.Scanner;
+
+public class Solution31 {
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+        /*
+        Take user input of:
+        age = in.newInt();
+        restingPulse = in.newInt()
+
+        intensity = 55 (loop) (55 + 5) until intensity = 95
+        obtain targetHeartRate:
+        targetHeartRate = (((220 - age) - restingHeartRate) * intensity) + restingHeartRate;
+
+         */
+    }
+    public static int[][] generateKarvonenHeartRate(){
+
+        return yes;
+    }
+}
