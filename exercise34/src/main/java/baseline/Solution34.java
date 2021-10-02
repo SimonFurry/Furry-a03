b5d9f9818a9b1c482544d9fd1fd305c84388a776
Simon <simonfurry@gmail,.com>
2021-10-02 18:08:49 -0400

    initial commit of exercise34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,22 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+import java.util.Scanner;
+
+public class Solution34 {
+    /*
+    Create a program that
+    contains a list of employee names.
+    print out the names when the program runs for the first time.
+    Prompt for an employee name and remove that name from the list.
+    display remaining employees.
+     */
+    public static void main(String[] args){
+
+    }
+    static void employeeListRemoval(){
+
+    }
+}
