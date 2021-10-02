5284b8aceeb17f4ad7222dc7d352c9386166a7d4
Simon <simonfurry@gmail,.com>
2021-10-02 15:20:58 -0400

    initial commit of exercise32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,35 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class Solution32 {
+    private static final Scanner in = new Scanner(System.in);
+    Random rand = new Random();
+
+    public static void main(String[] args){
+    guessTheNumber();
+    }
+    public static void guessTheNumber(){
+        /*
+        Create a random number generator:
+        level 1: 1-10
+        level 2: 1-100
+        level 3: 1-1000
+
+        prompt for difficulty level(1,2,3)
+        System.out.print("I have my number. What's your guess? ");
+        int num = in.nextInt();
+        if(num = randNum){
+        System.out.print("you got it in " + guesses + "guesses!");
+        }
+        if(num > randNum){
+        System.out.print("Too high. Guess again: ")
+
+        }
+         */
+    }
+}
