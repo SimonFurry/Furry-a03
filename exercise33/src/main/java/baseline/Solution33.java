f9c4cba6f58a63cf8df30a338eceb08c10f0978d
Simon <simonfurry@gmail,.com>
2021-10-02 17:29:56 -0400

    initial commit of exercise33

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,30 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Solution33 {
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+        magic8Ball();
+    }
+    public static void magic8Ball(){
+        /*
+        use an RNG to generate numbers 1-4
+        assign a print response to each number
+        if(num == 1){
+        System.out.print("Yes");
+        }
+        if(num == 2){
+        System.out.print("No")
+        }
+        ETC.
+        
+         */
+    }
+}
