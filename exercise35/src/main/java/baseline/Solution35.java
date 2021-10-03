bb4227c855ffa56449d40de2bb604c8449f8f753
Simon <simonfurry@gmail,.com>
2021-10-03 10:30:34 -0400

    initial commit of exercise35

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,28 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Solution35 {
+    /*
+Write a program that
+prompts user for input:
+if input is "" blank, break loop, run rng to determine winner
+if user input is "name" continue loop.
+ */
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+        winner();
+    }
+    static void winner(){
+        
+    }
+
+}
