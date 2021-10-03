0a72cae644355230b8921a5ff9e6329406b70d97
Simon <simonfurry@gmail,.com>
2021-10-03 17:10:12 -0400

    initial commit of exercise38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,17 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+import java.util.Scanner;
+
+public class Solution38 {
+    /*
+    write a program that
+    prompts for a list of numbers, separated by spaces
+    have program print out new list containing only the even numbers
+
+    filterEvenNumbers
+    
+     */
+}
