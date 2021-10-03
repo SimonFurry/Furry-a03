67ce815e446e3bc59fe8edb2290c192fb1553f0e
Simon <simonfurry@gmail,.com>
2021-10-03 11:35:39 -0400

    initial commit of exercise36

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,36 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Simon Furry
+ */
+
+import java.util.Scanner;
+
+public class Solution36 {
+    /*
+    write a program that
+    prompts for numbers
+    once input "done" complete functions
+    functions = average, max, min, std
+    average = numbersSum / (num.size() - 1);
+    max = Collections.max(list);
+    min = Collections.min(list);
+    std += Math.pow(num - mean, 2);
+     */
+    private static final Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
+
+    }
+    public static void average(){
+
+    }
+    public static void max(){
+
+    }
+    public static void min(){
+
+    }
+    public static void std(){
+        
+    }
+}
